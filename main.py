1868d5a769f6de3f4e6bcacfaa9930ebaef33e65
matheusmatta21 <138629402+matheusmatta21@users.noreply.github.com>
2023-12-14 16:42:58 -0300

    Versão 1: Validação não para o processo ainda

--- main.py
+++ main.py
@@ -1,61 +1,93 @@
-def pedirDados():
-    while True:
-        nome = input("Digite o nome (Digite 0 para encerrar.): ")
-        if nome == "0":
-            print("Finalizado.")
-            break
-        idade = int(input("Digite a idade: "))
-        while idade <=0 or idade >= 100:
-            idade = int(input("Digite uma idade válida: "))
-        sexo = input("Digite o sexo (M/F): ")
-        telefone = int(input("Digite o número de telefone: "))
-        salvarDados(nome,idade,sexo,telefone)
-        respostaBuscarPorSexo = input("Deseja realizar uma busca nos usuários de um sexo em específico (S/N)? ")
-        if respostaBuscarPorSexo == "S":
-            sexoDesejado = input("Digite o sexo em questão (M/F): ")
-            buscarPorSexo(sexoDesejado)
-        elif respostaBuscarPorSexo == "N":
-            print("Ok.")
-        respostaBuscarPorNome = input("Deseja realizar uma busca nos usuários com um nome ou caractéres específico (S/N)? ")
-        if respostaBuscarPorNome == "S":
-            caracteresDesejados = input("Digite o nome ou os caractéres em questão: ")
-            buscarPorNome(caracteresDesejados)
-        elif respostaBuscarPorNome == "N":
-            print("Ok.")
-            continue
+from tkinter import *
+from tkinter.ttk import Combobox
+from tkinter import messagebox 
+
+#funcoes
+
+def validar(nomeBase, quantidadeBase, precoBase, geladoBase):
+        if nomeBase == "" or nomeBase == " ":
+                messagebox.showerror(title="Erro", message="Digite um nome!")
+        elif quantidadeBase == "" or quantidadeBase == " " or quantidadeBase.isnumeric() == False:
+                messagebox.showerror(title="Erro", message="Digite um número na quantidade!")
+        elif precoBase == "" or precoBase == " " or precoBase.isnumeric() == False:
+                messagebox.showerror(title="Erro", message="Digite um número no preço!")
+        elif geladoBase == "" or geladoBase == " ":
+                messagebox.showerror(title="Erro", message="Selecione uma opção da caixa de seleções!")
+        
+
+def escreverProdutos(nomeBase, quantidadeBase, precoBase, geladoBase):
+    nome = inputNome.get()
+    quantidade = inputQuantidade.get()
+    preco = inputPreco.get()
+    gelado = inputCampo.get()
+    validar(nome, quantidade, preco, gelado)
+    arquivo = open("estoque.txt", "a", encoding="utf-8")
+    arquivo.write(f'{nomeBase},{quantidadeBase},{precoBase},{geladoBase}\n')
+    arquivo.close()
+        
+
+def verProdutos():
+    windowTwo = Tk()
+    windowTwo.title("Produtos")
+    windowTwo.geometry("1280x720")
+    arquivo = open("estoque.txt", "r", encoding="utf-8")
+    for linha in arquivo.readlines():
+        produto = linha.split(",")
+        labelProduto = Label(windowTwo, text=f"Nome: {produto[0]} | Quantidade: {produto[1]} | Preço: {produto[2]} | Gelado: {produto[3]}")
+        labelProduto.pack()
+    arquivo.close()
+
+def limparFormulario():
+     inputNome.delete(0, END)
+     inputQuantidade.delete(0, END)
+     inputPreco.delete(0, END)
+     inputCampo.delete(0, END)
+
+#gui
+
+window = Tk()
+window.title("Sistema de Controle de Estoque")
+window.geometry("600x300")
+
+#nome
+
+labelNome = Label(window, text="Nome:")
+labelNome.pack()
+inputNome = Entry(window, bd=2)
+inputNome.pack()
+
+#quantidade em estoque
+
+labelQuantidade = Label(window, text="Quantidade em estoque:")
+labelQuantidade.pack()
+inputQuantidade = Entry(window,bd=2)
+inputQuantidade.pack()
+
+#preço unitáro
+
+labelPreco = Label(window, text="Preço unitário:")
+labelPreco.pack()
+inputPreco = Entry(window,bd=2)
+inputPreco.pack()
+
+#campo de seleção
+
+labelCampo = Label(window, text="Gelado ou Não?")
+labelCampo.pack()
+values = ("Sim","Não") 
+inputCampo = Combobox(window, values=values)
+inputCampo.pack()
+
+#botões
+
+botaoAdicionar = Button(window, text="Adicionar", command= validar)
+botaoAdicionar.pack()
+botaoLimpar = Button(window, text="Limpar Formulário", command=limparFormulario)
+botaoLimpar.pack()
+botaoVisualizar = Button(window, text="Visualizar Produtos", command=verProdutos)
+botaoVisualizar.pack()
+
+#funcoes dos botoes
 
 
-def salvarDados(nome,idade,sexo,telefone):
-    with open("dados.txt", "a", encoding="utf-8") as arquivo:
-        arquivo.write(f"{nome}|{idade}|{sexo}|{telefone}\n")
-
-def printarDados():
-    with open("dados.txt", "r", encoding="utf-8") as arquivo:
-        for linhas in arquivo.readlines():
-            listaUsuarios = linhas.split("|")
-            if listaUsuarios[2] == "M":
-                listaUsuarios[2] = "Masculino"
-            elif listaUsuarios[2] == "F":
-                 listaUsuarios[2] = "Feminino"
-            print(f"Nome: {listaUsuarios[0]}\Idade: {listaUsuarios[1]}\nSexo: {listaUsuarios[2]}\nTelefone: {listaUsuarios[3]}\n")
-
-def buscarPorSexo(sexoDesejado):
-    with open("dados.txt", "r", encoding="utf-8") as arquivo:
-        for linhas in arquivo.readlines():
-            listaUsuarios = linhas.split("|")
-            if listaUsuarios[2] == sexoDesejado:
-                print(f"Nome: {listaUsuarios[0]}\Idade: {listaUsuarios[1]}\nSexo: {listaUsuarios[2]}\nTelefone: {listaUsuarios[3]}\n")
-            else:
-                continue
-
-def buscarPorNome(caracteresDesejados):
-    with open("dados.txt", "r", encoding="utf-8") as arquivo:
-        for linhas in arquivo.readlines():
-            listaUsuarios = linhas.split("|")
-            if caracteresDesejados in listaUsuarios[0]:
-                print(f"Nome: {listaUsuarios[0]}\Idade: {listaUsuarios[1]}\nSexo: {listaUsuarios[2]}\nTelefone: {listaUsuarios[3]}\n")
-            else:
-                continue
-
-if __name__ == "__main__":
-    pedirDados()
+window.mainloop()
